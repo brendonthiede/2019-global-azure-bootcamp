73ac589ab364a8c6e71e3293f038d6f43e04a369
Brendon Thiede <brendon.thiede@gmail.com>
2019-04-27 14:48:02 -0400

    Removing junk

--- stage-03/New-ApplicationDeployment.ps1
+++ stage-03/New-ApplicationDeployment.ps1
@@ -27,28 +27,6 @@ trap {
 
 Push-Location
 
-Write-Verbose "Installing Application dependencies"
-Set-Location $PSScriptRoot/Application
-npm install
-
-Write-Verbose "Installing Tests dependencies"
-Set-Location $PSScriptRoot/Tests
-npm install
-
-Write-Verbose "Running Unit Tests"
-Set-Location $PSScriptRoot/Tests
-./node_modules/.bin/gulp unittest
-if ($LASTEXITCODE -ne 0) {
-    throw "Unit tests failed"
-}
-
-Write-Verbose "Packaging application code"
-$distFolder = "$PSScriptRoot/dist"
-$zipFilePath = "$distFolder/application.zip"
-$appFolder = "$PSScriptRoot/Application"
-New-Item -Path $distFolder -ItemType Directory -ErrorAction SilentlyContinue -Force
-Compress-Archive -Path "$appFolder/*" -DestinationPath $zipFilePath -Force
-
 $randomPassword = (Get-BootcampRandomPassword)
 $myIP = (Invoke-RestMethod https://ipinfo.io/ip).Trim()
 $sqlServerAdminGroup = (New-BootcampSQLServerAdminGroup)
@@ -69,28 +47,5 @@ $outputs = (New-BootcampResourceGroupDeployment `
         -Location $Location `
         -Force:$Force `
         -Verbose:$VerbosePreference).Outputs
-
-$siteUrl = "https://$($outputs.defaultHostName.value)"
-Write-Verbose "Waiting for $siteUrl to be available before code deployment"
-Set-Location $PSScriptRoot/Tests
-./node_modules/.bin/wait-on $siteUrl
-
-New-BootcampWebAppDeployment `
-    -ZipFilePath $zipFilePath `
-    -WebAppName $outputs.webAppName.value `
-    -ResourceGroupName $ResourceGroupName `
-    -Verbose:$VerbosePreference
-
-$siteUrl = "https://$($outputs.defaultHostName.value)"
-Write-Verbose "Waiting for $siteUrl to be available after code deployment"
-Set-Location $PSScriptRoot/Tests
-./node_modules/.bin/wait-on $siteUrl
-
-Write-Verbose "Running functional tests against $siteUrl"
-Set-Location $PSScriptRoot/Tests
-./node_modules/.bin/gulp functionaltest --webAppUrl "$siteUrl"
-if ($LASTEXITCODE -ne 0) {
-    throw "Functional tests failed"
-}
 
 Pop-Location
